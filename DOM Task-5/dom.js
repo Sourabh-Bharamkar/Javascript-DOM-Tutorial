a87762d65db798692a2eddf835434226c46516cd
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2022-12-17 23:21:13 +0530

    added new <li> element edited it by using getElementsByTagName and getElementsByClassName

---
+++ DOM Task-5/dom.js
@@ -0,0 +1,6 @@
+let items=document.getElementsByClassName('list-item')
+items[0].style.color='red'
+items=document.getElementsByTagName('li')
+console.log(items)
+items[4].style.backgroundColor='yellow'
+
