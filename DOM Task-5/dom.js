c2b669717b452e3ca8e65f31297ee2c76fff6dbc
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2022-12-17 23:27:50 +0530

    new <li> element added apply getElementsByTagName and getElementsByClassName

--- DOM Task-5/dom.js
+++ DOM Task-5/dom.js
@@ -1,5 +1,8 @@
+
 let items=document.getElementsByClassName('list-item')
 items[0].style.color='red'
+
+
 items=document.getElementsByTagName('li')
 console.log(items)
 items[4].style.backgroundColor='yellow'
